64b563ec50e3e996a22eb84cc02bd2173b9eea0b
DongYoung <bigdykim@gmail.com>
2023-07-11 11:46:39 +0900

    WILDCARD improve O() solve

--- algorithm-problem-solving-strategies/WILDCARD/improve_final.cpp
+++ algorithm-problem-solving-strategies/WILDCARD/improve_final.cpp
@@ -12,17 +12,19 @@ bool match (int pi, int ti)
 // p와 t의 사작 index를 전단 받도록 수정
 {
     // 1. 기저사례 처리
-    // (이 경우 기저사례는 아래 계산 과정에서 처리된다)
+    // (이 문제의 경우 기저사레는 아래 계산 과정에서 처리된다)
 
     // 2. 캐시확인
     int & ret = cache[pi][ti];
     if (ret != -1) return ret;
 
     // 3. 계산
-    for (pi, ti; pi < p.size() && ti < t.size(); pi++, ti++)
-    // pos 변수 대신 pi와 ti를 사용
+    if(pi < p.size() 
+        && ti < t.size() 
+        && (t[ti] == p[pi] || '?' == p[pi])) 
+    // 한 글자가 범위 내로 매칭이 가능하다면 다음 글자를 검사하고 그 결과를 반환한다.
     {
-        if (t[ti] != p[pi] && '?' != p[pi]) break;
+        return ret = match(pi+1, ti+1);
     }
 
     // 패턴 끝 도달
@@ -35,10 +37,12 @@ bool match (int pi, int ti)
 
     if (p[pi] == '*')
     {
-        // k : ti ~ t.size()
-        for (int k = ti; k <= t.size(); k++)
+        if(match(pi+1, ti)|| match(pi, ti+1))
+        // 여기에서 범위 검사를 해주어야 하는가?
+        // 1. pi가 끝에 도달한다면? 재귀함수 pi 범위 검사에서 걸러짐 -> 문제없음
+        // 2. ti가 끝에 도달한다면 -> 모든 if 문에 포함되지 못하고 0을 return -> 문제없음
         {
-            if(match(pi+1, k)) return ret = 1;
+            return ret = 1;
         }
     }
     return ret = 0;
