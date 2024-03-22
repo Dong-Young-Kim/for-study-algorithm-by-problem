34847f24fda99253fb30c86219fe3dec96f9e7ca
Dong-Young-Kim <bigdykim@gmail.com>
2024-03-23 00:49:34 +0900

    refactor boj 7576

--- baekjoon_online_judge/토마토/boj_7576.py
+++ baekjoon_online_judge/토마토/boj_7576.py
@@ -1,3 +1,5 @@
+# boj_7576 : 토마토
+
 import sys
 from collections import deque
 input = sys.stdin.readline
@@ -11,23 +13,21 @@ q = deque()
 for i in range(N*M):
     if box[i // M][i % M] == 1: q.append((i // M, i % M))
 
+# bfs
 while(q):
     x, y = q.popleft()
+    day = box[x][y]
     for d in delta_pos:
         nx, ny = x + d[0], y + d[1]
         if nx < 0 or nx >= N or ny < 0 or ny >= M : continue # out of range
-        # 여러개의 익은 토마토가 있을 수 있으므로
-        # 익는데 더 적은 일수가 걸리는 토마토가 있다면 새로 더 적은 일수로 업데이트 한다.
-        if box[nx][ny] == 0 or box[nx][ny] > box[x][y] + 1:
-            box[nx][ny] = box[x][y] + 1
+        if not box[nx][ny]:
+            box[nx][ny] = day + 1
             q.append((nx, ny))
 
-maxR = 1
-for i in range(N*M):
+# find not Ripped
+for i in range(N * M):
     x, y = i // M, i % M
-    if box[x][y] == 0:
-        maxR = 0
+    if not box[x][y]:
+        day = 0
         break
-    if box[x][y] > maxR:
-        maxR = box[x][y]
-print(maxR - 1)
+print(day - 1)
